d790a319ccf22272fa06ed2f0f77ba8c25af2df1
fishka9044 <79632566+fishka9044@users.noreply.github.com>
2021-11-28 19:55:54 +0300

    push

---
+++ js/index.js
@@ -0,0 +1,37 @@
+let listPlaceList = document.querySelector(".menu__list");
+let allLink = listPlaceList.querySelectorAll(".menu__link-icon")
+
+$(document).ready(function () {
+  $(".slider__down").slick({
+      arrows:false,
+      dots:false,
+      autoplay:false,
+      infinity: true,
+      autoplaySpeed:500,
+      slidesToShow: 4,
+      centerMode: false,
+      draggable: false,
+      variableWidth: true,
+      responsive: [
+        {
+          breakpoint: 1250,
+          settings: {
+            slidesToShow: 3,
+            arrows:true,
+            centerMode: false,
+            variableWidth: true,
+          }
+        },
+        {
+          breakpoint: 701,
+          settings: {
+            slidesToShow: 1,
+            arrows:true,
+            centerMode: false,
+            variableWidth: true,
+          }
+        }
+      ]
+  });
+});
+
